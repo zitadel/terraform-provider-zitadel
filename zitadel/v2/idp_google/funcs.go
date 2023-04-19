359380ce41ee536762cb052081c8c0caa9bc0749
Elio Bischof <eliobischof@gmail.com>
2023-04-19 11:09:07 +0200

    feat: add Google org idp

---
+++ zitadel/v2/idp_google/funcs.go
@@ -0,0 +1,111 @@
+package idp_google
+
+import (
+	"context"
+
+	"github.com/hashicorp/terraform-plugin-sdk/v2/diag"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
+	"github.com/zitadel/zitadel-go/v2/pkg/client/zitadel/admin"
+	"github.com/zitadel/zitadel-go/v2/pkg/client/zitadel/idp"
+
+	"github.com/zitadel/terraform-provider-zitadel/zitadel/v2/helper"
+	"github.com/zitadel/terraform-provider-zitadel/zitadel/v2/idp_utils"
+)
+
+func create(ctx context.Context, d *schema.ResourceData, m interface{}) diag.Diagnostics {
+	clientinfo, ok := m.(*helper.ClientInfo)
+	if !ok {
+		return diag.Errorf("failed to get client")
+	}
+	client, err := helper.GetAdminClient(clientinfo)
+	if err != nil {
+		return diag.FromErr(err)
+	}
+	resp, err := client.AddGoogleProvider(ctx, &admin.AddGoogleProviderRequest{
+		Name:         d.Get(idp_utils.NameVar).(string),
+		ClientId:     d.Get(idp_utils.ClientIDVar).(string),
+		ClientSecret: d.Get(idp_utils.ClientSecretVar).(string),
+		Scopes:       helper.GetOkSetToStringSlice(d, idp_utils.ScopesVar),
+		ProviderOptions: &idp.Options{
+			IsLinkingAllowed:  d.Get(idp_utils.IsLinkingAllowedVar).(bool),
+			IsCreationAllowed: d.Get(idp_utils.IsCreationAllowedVar).(bool),
+			IsAutoUpdate:      d.Get(idp_utils.IsAutoUpdateVar).(bool),
+			IsAutoCreation:    d.Get(idp_utils.IsAutoCreationVar).(bool),
+		},
+	})
+	if err != nil {
+		return diag.Errorf("failed to create idp: %v", err)
+	}
+	d.SetId(resp.GetId())
+	return nil
+}
+
+func update(ctx context.Context, d *schema.ResourceData, m interface{}) diag.Diagnostics {
+	clientinfo, ok := m.(*helper.ClientInfo)
+	if !ok {
+		return diag.Errorf("failed to get client")
+	}
+	client, err := helper.GetAdminClient(clientinfo)
+	if err != nil {
+		return diag.FromErr(err)
+	}
+	if d.HasChangesExcept(idp_utils.IdpIDVar) {
+		_, err = client.UpdateGoogleProvider(ctx, &admin.UpdateGoogleProviderRequest{
+			Id:           d.Id(),
+			Name:         d.Get(idp_utils.NameVar).(string),
+			ClientId:     d.Get(idp_utils.ClientIDVar).(string),
+			ClientSecret: d.Get(idp_utils.ClientSecretVar).(string),
+			Scopes:       helper.GetOkSetToStringSlice(d, idp_utils.ScopesVar),
+			ProviderOptions: &idp.Options{
+				IsLinkingAllowed:  d.Get(idp_utils.IsLinkingAllowedVar).(bool),
+				IsCreationAllowed: d.Get(idp_utils.IsCreationAllowedVar).(bool),
+				IsAutoCreation:    d.Get(idp_utils.IsAutoCreationVar).(bool),
+				IsAutoUpdate:      d.Get(idp_utils.IsAutoUpdateVar).(bool),
+			},
+		})
+		if err != nil {
+			return diag.Errorf("failed to update idp: %v", err)
+		}
+	}
+	return nil
+}
+
+func read(ctx context.Context, d *schema.ResourceData, m interface{}) diag.Diagnostics {
+	clientinfo, ok := m.(*helper.ClientInfo)
+	if !ok {
+		return diag.Errorf("failed to get client")
+	}
+	client, err := helper.GetAdminClient(clientinfo)
+	if err != nil {
+		return diag.FromErr(err)
+	}
+	resp, err := client.GetProviderByID(ctx, &admin.GetProviderByIDRequest{Id: helper.GetID(d, idp_utils.IdpIDVar)})
+	if err != nil && helper.IgnoreIfNotFoundError(err) == nil {
+		d.SetId("")
+		return nil
+	}
+	if err != nil {
+		return diag.Errorf("failed to get idp")
+	}
+	idp := resp.GetIdp()
+	cfg := idp.GetConfig()
+	specificCfg := cfg.GetGoogle()
+	generalCfg := cfg.GetOptions()
+	set := map[string]interface{}{
+		idp_utils.NameVar:              idp.GetName(),
+		idp_utils.ClientIDVar:          specificCfg.GetClientId(),
+		idp_utils.ClientSecretVar:      d.Get(idp_utils.ClientSecretVar).(string),
+		idp_utils.ScopesVar:            specificCfg.GetScopes(),
+		idp_utils.IsLinkingAllowedVar:  generalCfg.GetIsLinkingAllowed(),
+		idp_utils.IsCreationAllowedVar: generalCfg.GetIsCreationAllowed(),
+		idp_utils.IsAutoCreationVar:    generalCfg.GetIsAutoCreation(),
+		idp_utils.IsAutoUpdateVar:      generalCfg.GetIsAutoUpdate(),
+	}
+	for k, v := range set {
+		if err := d.Set(k, v); err != nil {
+			return diag.Errorf("failed to set %s of oidc idp: %v", k, err)
+		}
+	}
+	d.SetId(idp.Id)
+	return nil
+}
---
+++ zitadel/v2/idp_google/resource_test.go
@@ -0,0 +1,29 @@
+package idp_google_test
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/zitadel/terraform-provider-zitadel/zitadel/v2/idp_utils/test_utils"
+)
+
+func TestAccZITADELInstanceIdPGoogle(t *testing.T) {
+	resourceName := "zitadel_idp_google"
+	frame, err := test_utils.NewInstanceTestFrame(resourceName)
+	if err != nil {
+		t.Fatalf("setting up test context failed: %v", err)
+	}
+	test_utils.RunBasicLifecyleTest(t, frame, func(name, secret string) string {
+		return fmt.Sprintf(`
+resource "%s" "%s" {
+  name                = "%s"
+  client_id           = "aclientid"
+  client_secret       = "%s"
+  scopes              = ["two", "scopes"]
+  is_linking_allowed  = false
+  is_creation_allowed = true
+  is_auto_creation    = false
+  is_auto_update      = true
+}`, resourceName, frame.UniqueResourcesID, name, secret)
+	})
+}
