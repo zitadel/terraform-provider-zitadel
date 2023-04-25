eb807f380cf337aad56a38334287a72bd309a6d6
Elio Bischof <eliobischof@gmail.com>
2023-04-25 18:26:43 +0200

    fix tenant type schema

--- zitadel/v2/idp_azure_ad/schema.go
+++ zitadel/v2/idp_azure_ad/schema.go
@@ -3,7 +3,12 @@ package idp_azure_ad
 import (
 	"fmt"
 
+	"github.com/hashicorp/go-cty/cty"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/diag"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
+	"github.com/zitadel/zitadel-go/v2/pkg/client/zitadel/idp"
+
+	"github.com/zitadel/terraform-provider-zitadel/zitadel/v2/helper"
 )
 
 const (
@@ -16,8 +21,11 @@ var (
 	TenantTypeResourceField = &schema.Schema{
 		Type:        schema.TypeString,
 		Optional:    true,
-		Default:     "AZURE_AD_TENANT_TYPE_COMMON",
+		Default:     idp.AzureADTenantType_name[0],
 		Description: "the azure ad tenant type",
+		ValidateDiagFunc: func(value interface{}, path cty.Path) diag.Diagnostics {
+			return helper.EnumValueValidation(TenantTypeVar, value, idp.AzureADTenantType_value)
+		},
 	}
 	TenantTypeDataSourceField = &schema.Schema{
 		Type:        schema.TypeString,
