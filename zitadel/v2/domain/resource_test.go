8c551fe811190bc2b4fa30deec64e920faac548d
Elio Bischof <eliobischof@gmail.com>
2023-07-19 18:58:01 +0200

    test domain

---
+++ zitadel/v2/domain/resource_test.go
@@ -0,0 +1,82 @@
+package domain_test
+
+import (
+	"errors"
+	"fmt"
+	"regexp"
+	"testing"
+
+	"github.com/zitadel/zitadel-go/v2/pkg/client/zitadel/org"
+
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/terraform"
+	"github.com/zitadel/zitadel-go/v2/pkg/client/zitadel/management"
+
+	"github.com/zitadel/terraform-provider-zitadel/zitadel/v2/helper/test_utils"
+)
+
+func TestAccZITADELDomain(t *testing.T) {
+	resourceName := "zitadel_domain"
+	initialProperty := "initial.default.127.0.0.1.sslip.io"
+	updatedProperty := "updated.default.127.0.0.1.sslip.io"
+	frame, err := test_utils.NewOrgTestFrame(resourceName)
+	if err != nil {
+		t.Fatalf("setting up test context failed: %v", err)
+	}
+	test_utils.RunLifecyleTest(
+		t,
+		frame.BaseTestFrame,
+		func(configProperty, _ interface{}) string {
+			return fmt.Sprintf(`
+resource "%s" "%s" {
+  org_id          = "%s"
+  name      = "%s"
+  is_primary = false
+}`, resourceName, frame.UniqueResourcesID, frame.OrgID, configProperty)
+		},
+		initialProperty, updatedProperty,
+		"", "",
+		checkRemoteProperty(frame),
+		regexp.MustCompile(fmt.Sprintf(`^%s$|^%s$`, initialProperty, updatedProperty)),
+		checkDestroy(frame, updatedProperty),
+		nil, nil, "", "",
+	)
+}
+
+var ErrNotFound = errors.New("not found")
+
+func checkRemoteProperty(frame *test_utils.OrgTestFrame) func(interface{}) resource.TestCheckFunc {
+	return func(expect interface{}) resource.TestCheckFunc {
+		return func(_ *terraform.State) error {
+			remoteResource, err := frame.ListOrgDomains(frame, &management.ListOrgDomainsRequest{
+				Queries: []*org.DomainSearchQuery{{
+					Query: &org.DomainSearchQuery_DomainNameQuery{
+						DomainNameQuery: &org.DomainNameQuery{
+							Name: expect.(string),
+						},
+					},
+				}},
+			})
+			if err != nil {
+				return err
+			}
+			if len(remoteResource.GetResult()) == 0 {
+				return fmt.Errorf("expected to find %s, but didn't: %w", expect, ErrNotFound)
+			}
+			return nil
+		}
+	}
+}
+
+func checkDestroy(frame *test_utils.OrgTestFrame, expectNotFound string) resource.TestCheckFunc {
+	return func(state *terraform.State) error {
+		err := checkRemoteProperty(frame)(expectNotFound)(state)
+		if errors.Is(err, ErrNotFound) {
+			return nil
+		}
+		if err == nil {
+			return fmt.Errorf("expected to not find %s, but did", expectNotFound)
+		}
+		return err
+	}
+}
