457892af880654ee074e6eae7b49e20e1f2f6f7e
Elio Bischof <elio@zitadel.com>
2024-02-22 09:51:46 +0100

    docs: import

--- examples/provider/resources/machine_user-import.sh
+++ examples/provider/resources/machine_user-import.sh
@@ -1,2 +1,2 @@
-# The resource can be imported using the ID format `<user_id:with_secret[:org_id][:client_id][:client_secret]>`, e.g.
+# The resource can be imported using the ID format `<id:has_secret[:org_id][:client_id][:client_secret]>`, e.g.
 terraform import machine_user.imported '123456789012345678:123456789012345678:true:my-machine-user:j76mh34CHVrGGoXPQOg80lch67FIxwc2qIXjBkZoB6oMbf31eGMkB6bvRyaPjR2t'
