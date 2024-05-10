01f479966e391b3f9b582f95f33c7ff95203de84
Elio Bischof <elio@zitadel.com>
2024-05-10 06:37:15 +0200

    fix typos

--- examples/provider/resources/org_metadata-import.sh
+++ examples/provider/resources/org_metadata-import.sh
@@ -1,2 +1,2 @@
 # The resource can be imported using the ID format `<key[:org_id]>`, e.g.
-terraform import org_member.imported 'a_key:123456789012345678'
+terraform import org_metadata.imported 'a_key:123456789012345678'
