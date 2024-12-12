ffec5e75c330b6b4167b086de709adda647796cf
David Fry <david.fry@modusbox.com>
2024-12-12 00:52:33 +0000

    cleanup based on stebanz comments

--- zitadel/user_grant/funcs.go
+++ zitadel/user_grant/funcs.go
@@ -128,167 +128,84 @@ func read(ctx context.Context, d *schema.ResourceData, m interface{}) diag.Diagn
 }
 
 func readDS(ctx context.Context, d *schema.ResourceData, m interface{}) diag.Diagnostics {
-
 	tflog.Info(ctx, "started read")
-
 	clientinfo, ok := m.(*helper.ClientInfo)
-
 	if !ok {
-
 		return diag.Errorf("failed to get client")
-
 	}
-
 	client, err := helper.GetManagementClient(ctx, clientinfo)
-
 	if err != nil {
-
 		return diag.FromErr(err)
-
 	}
-
 	resp, err := client.GetUserGrantByID(helper.CtxWithOrgID(ctx, d), &management.GetUserGrantByIDRequest{
-
 		GrantId: helper.GetID(d, grantIDVar),
-
-		UserId: d.Get(UserIDVar).(string),
+		UserId:  d.Get(UserIDVar).(string),
 	})
-
 	if err != nil && helper.IgnoreIfNotFoundError(err) == nil {
-
 		d.SetId("")
-
 		return nil
-
 	}
-
 	if err != nil {
-
 		return diag.Errorf("failed to get user grant")
-
 	}
-
 	grant := resp.GetUserGrant()
-
 	set := map[string]interface{}{
-
-		UserIDVar: grant.GetUserId(),
-
-		RoleKeysVar: grant.GetRoleKeys(),
-
-		userNameVar: grant.GetUserName(),
-
-		roleStatusVar: grant.GetState().String(),
-
-		projectNameVar: grant.GetProjectName(),
-
-		emailVar: grant.Email,
-
-		nameVar: grant.DisplayName,
+		UserIDVar:         grant.GetUserId(),
+		RoleKeysVar:       grant.GetRoleKeys(),
+		userNameVar:       grant.GetUserName(),
+		roleStatusVar:     grant.GetState().String(),
+		projectNameVar:    grant.GetProjectName(),
+		emailVar:          grant.Email,
+		nameVar:           grant.DisplayName,
+		projectIDVar:      grant.GetProjectId(),
+		projectGrantIDVar: grant.GetProjectGrantId(),
 	}
-
-	if grant.GetProjectId() != "" {
-
-		set[projectIDVar] = grant.GetProjectId()
-
-	}
-
-	if grant.GetProjectGrantId() != "" {
-
-		set[projectGrantIDVar] = grant.GetProjectGrantId()
-
-	}
-
 	for k, v := range set {
-
 		if err := d.Set(k, v); err != nil {
-
 			return diag.Errorf("failed to set %s of usergrant: %v", k, err)
-
 		}
-
 	}
-
 	d.SetId(grant.GetId())
-
 	return nil
-
 }
-
 func list(ctx context.Context, d *schema.ResourceData, m interface{}) diag.Diagnostics {
-
 	tflog.Info(ctx, "started list")
-
 	projectName := d.Get(projectNameVar).(string)
-
 	clientinfo, ok := m.(*helper.ClientInfo)
-
 	if !ok {
-
 		return diag.Errorf("failed to get client")
-
 	}
-
 	client, err := helper.GetManagementClient(ctx, clientinfo)
-
 	if err != nil {
-
 		return diag.FromErr(err)
-
 	}
-
 	req := &management.ListUserGrantRequest{}
-
 	req.Queries = append(req.Queries, &user.UserGrantQuery{
-
 		Query: &user.UserGrantQuery_ProjectNameQuery{
-
 			ProjectNameQuery: &user.UserGrantProjectNameQuery{
-
 				ProjectName: projectName,
-
-				Method: 3,
+				Method:      3,
 			},
 		},
 	})
-
 	resp, err := client.ListUserGrants(ctx, req)
-
 	if err != nil {
-
 		return diag.Errorf("error while getting roles by projName %s: %v", projectName, err)
-
 	}
-
 	results := []map[string]interface{}{}
-
 	for _, roleGrant := range resp.Result {
-
 		results = append(results, map[string]interface{}{
-
-			UserIDVar: roleGrant.UserId,
-
-			grantIDVar: roleGrant.Id,
-
-			RoleKeysVar: roleGrant.GetRoleKeys(),
-
-			userNameVar: roleGrant.GetUserName(),
-
-			roleStatusVar: roleGrant.GetState().String(),
-
+			UserIDVar:      roleGrant.UserId,
+			grantIDVar:     roleGrant.Id,
+			RoleKeysVar:    roleGrant.GetRoleKeys(),
+			userNameVar:    roleGrant.GetUserName(),
+			roleStatusVar:  roleGrant.GetState().String(),
 			projectNameVar: roleGrant.GetProjectName(),
-
-			emailVar: roleGrant.Email,
-
-			nameVar: roleGrant.DisplayName,
+			emailVar:       roleGrant.Email,
+			nameVar:        roleGrant.DisplayName,
 		})
-
 	}
-
 	// If the ID is blank, the datasource is deleted and not usable.
-
 	d.SetId("-")
-
 	return diag.FromErr(d.Set(userGrantDataVar, results))
-
 }
