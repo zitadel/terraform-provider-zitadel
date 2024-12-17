83907f4cb24dd155a10de610d7bbb9e6b78cd1fe
Stefan Benz <46600784+stebenz@users.noreply.github.com>
2024-12-17 11:03:10 +0100

    fix: add auto linking to org idps

--- examples/provider/resources/org_idp_ldap.tf
+++ examples/provider/resources/org_idp_ldap.tf
@@ -17,6 +17,7 @@ resource "zitadel_org_idp_ldap" "default" {
   is_creation_allowed  = true
   is_auto_creation     = false
   is_auto_update       = true
+  auto_linking        = "AUTO_LINKING_OPTION_USERNAME"
 }
 
 
