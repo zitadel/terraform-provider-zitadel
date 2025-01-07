5927372eef7c4fedad0f516fa18bc87e3312b3c2
Stefan Benz <46600784+stebenz@users.noreply.github.com>
2025-01-07 14:12:51 +0100

    fix: rename datasource for consistency

--- examples/provider/data-sources/org_idp_jwt.tf
+++ examples/provider/data-sources/org_idp_jwt.tf
@@ -1,8 +1,8 @@
-data "zitadel_org_jwt_idp" "default" {
+data "zitadel_org_idp_jwt" "default" {
   org_id = data.zitadel_org.default.id
   id     = "123456789012345678"
 }
 
-output "org_idp_org_jwt_idp" {
+output "org_idp_org_idp_jwt" {
   value = data.zitadel_org_jwt_idp.default
 }
