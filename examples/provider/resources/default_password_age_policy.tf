3096f61f0af42f7b2412a65c4715fcd0bb23166d
Elio Bischof <elio@zitadel.com>
2025-05-13 08:58:04 +0200

    int

--- examples/provider/resources/default_password_age_policy.tf
+++ examples/provider/resources/default_password_age_policy.tf
@@ -1,4 +1,4 @@
 resource "zitadel_default_password_age_policy" "default" {
-  max_age_days     = "30"
-  expire_warn_days = "5"
+  max_age_days     = 30
+  expire_warn_days = 5
 }
--- examples/provider/resources/password_age_policy.tf
+++ examples/provider/resources/password_age_policy.tf
@@ -1,5 +1,5 @@
 resource "zitadel_password_age_policy" "default" {
   org_id           = data.zitadel_org.default.id
-  max_age_days     = "30"
-  expire_warn_days = "5"
+  max_age_days     = 30
+  expire_warn_days = 5
 }
