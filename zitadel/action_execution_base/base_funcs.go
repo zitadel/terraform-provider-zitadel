8c4c25e0c925ef3abecc263ff5ab3e879df6f63e
Mridang Agarwalla <mridang.agarwalla@gmail.com>
2025-11-14 11:09:43 +0200

    refactor: remove unused ConditionFromID helper

--- zitadel/action_execution_base/base_funcs.go
+++ zitadel/action_execution_base/base_funcs.go
@@ -2,8 +2,6 @@ package action_execution
 
 import (
 	"context"
-	"fmt"
-	"strings"
 
 	"github.com/hashicorp/terraform-plugin-log/tflog"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/diag"
@@ -54,63 +52,4 @@ func ReadExecutionBase(ctx context.Context, d *schema.ResourceData, m interface{
 
 	d.SetId("")
 	return nil, nil
-}
-
-func ConditionFromID(id string) (*action.Condition, error) {
-	parts := strings.SplitN(id, "/", 2)
-	if len(parts) == 0 {
-		return nil, fmt.Errorf("invalid execution ID format: %s", id)
-	}
-
-	conditionType := parts[0]
-	value := ""
-	if len(parts) == 2 {
-		value = parts[1]
-	}
-
-	if conditionType == "request" && strings.HasPrefix(id, "request/") {
-		value = "/" + value
-	}
-	if conditionType == "response" && strings.HasPrefix(id, "response/") {
-		value = "/" + value
-	}
-
-	condition := &action.Condition{}
-	switch conditionType {
-	case "request":
-		req := &action.RequestExecution{}
-		if value == "" {
-			req.Condition = &action.RequestExecution_All{All: true}
-		} else if strings.HasPrefix(value, "/") {
-			req.Condition = &action.RequestExecution_Method{Method: value}
-		} else {
-			req.Condition = &action.RequestExecution_Service{Service: value}
-		}
-		condition.ConditionType = &action.Condition_Request{Request: req}
-	case "response":
-		resp := &action.ResponseExecution{}
-		if value == "" {
-			resp.Condition = &action.ResponseExecution_All{All: true}
-		} else if strings.HasPrefix(value, "/") {
-			resp.Condition = &action.ResponseExecution_Method{Method: value}
-		} else {
-			resp.Condition = &action.ResponseExecution_Service{Service: value}
-		}
-		condition.ConditionType = &action.Condition_Response{Response: resp}
-	case "function":
-		condition.ConditionType = &action.Condition_Function{Function: &action.FunctionExecution{Name: value}}
-	case "event":
-		event := &action.EventExecution{}
-		if value == "" {
-			event.Condition = &action.EventExecution_All{All: true}
-		} else if strings.HasSuffix(value, ".*") {
-			event.Condition = &action.EventExecution_Group{Group: value}
-		} else {
-			event.Condition = &action.EventExecution_Event{Event: value}
-		}
-		condition.ConditionType = &action.Condition_Event{Event: event}
-	default:
-		return nil, fmt.Errorf("unknown condition type in ID: %s", conditionType)
-	}
-	return condition, nil
 }
