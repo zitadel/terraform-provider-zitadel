0ff43fb57119142bab7aba1a6a16b7c8714672e3
Mridang Agarwalla <mridang.agarwalla@gmail.com>
2025-12-31 14:08:05 +1100

    chore: reformat all .tf files to use space-based indentation (#323)

--- examples/provider/data-sources/project_roles.tf
+++ examples/provider/data-sources/project_roles.tf
@@ -1,9 +1,9 @@
 data "zitadel_project_roles" "default" {
-    org_id     = data.zitadel_org.default.id
-    project_id = data.zitadel_project.default.id
-    role_key   = "admin"
+  org_id     = data.zitadel_org.default.id
+  project_id = data.zitadel_project.default.id
+  role_key   = "admin"
 }
 
 output "project_roles" {
-    value = data.zitadel_project_roles.default
+  value = data.zitadel_project_roles.default
 }
--- examples/provider/resources/instance_member.tf
+++ examples/provider/resources/instance_member.tf
@@ -1,4 +1,4 @@
 resource "zitadel_instance_member" "default" {
   user_id = data.zitadel_human_user.default.id
-  roles   = ["IAM_OWNER"]
+  roles = ["IAM_OWNER"]
 }
