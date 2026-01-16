89a16b84ef215cbc63875c54450f0e8243ff5277
Mridang Agarwalla <mridang.agarwalla@gmail.com>
2026-01-16 16:26:46 +1100

    fix: restore machine user secret generation and persist access token type (#343)

--- zitadel/machine_user/resource_test.go
+++ zitadel/machine_user/resource_test.go
@@ -15,7 +15,7 @@ import (
 	"github.com/zitadel/terraform-provider-zitadel/v2/zitadel/machine_user"
 )
 
-func TestAccMachineUserWithoutSecret(t *testing.T) {
+func TestAccMachineUserBearerWithoutSecret(t *testing.T) {
 	frame := test_utils.NewOrgTestFrame(t, "zitadel_machine_user")
 	resourceExample, exampleAttributes := test_utils.ReadExample(t, test_utils.Resources, frame.ResourceType)
 	exampleUsername := test_utils.AttributeValue(t, machine_user.UserNameVar, exampleAttributes).AsString()
@@ -42,7 +42,7 @@ func TestAccMachineUserWithoutSecret(t *testing.T) {
 	)
 }
 
-func TestAccMachineUserWithSecret(t *testing.T) {
+func TestAccMachineUserBearerWithSecret(t *testing.T) {
 	frame := test_utils.NewOrgTestFrame(t, "zitadel_machine_user")
 	resourceExample, exampleAttributes := test_utils.ReadExample(t, test_utils.Resources, frame.ResourceType)
 	exampleUsername := test_utils.AttributeValue(t, machine_user.UserNameVar, exampleAttributes).AsString()
@@ -58,6 +58,65 @@ func TestAccMachineUserWithSecret(t *testing.T) {
 		"", "true", "true",
 		false,
 		checkRemotePropertyWithSecret(frame),
+		helper.ZitadelGeneratedIdOnlyRegex,
+		test_utils.CheckIsNotFoundFromPropertyCheck(checkRemoteProperty(frame), ""),
+		test_utils.ChainImportStateIdFuncs(
+			test_utils.ImportResourceId(frame.BaseTestFrame),
+			func(state *terraform.State) (string, error) {
+				return strconv.FormatBool(true), nil
+			},
+			test_utils.ImportOrgId(frame),
+			test_utils.ImportStateAttribute(frame.BaseTestFrame, "client_id"),
+			test_utils.ImportStateAttribute(frame.BaseTestFrame, "client_secret"),
+		),
+	)
+}
+
+func TestAccMachineUserJWTWithoutSecret(t *testing.T) {
+	frame := test_utils.NewOrgTestFrame(t, "zitadel_machine_user")
+	resourceExample, exampleAttributes := test_utils.ReadExample(t, test_utils.Resources, frame.ResourceType)
+	exampleUsername := test_utils.AttributeValue(t, machine_user.UserNameVar, exampleAttributes).AsString()
+	resourceExample = strings.Replace(resourceExample, exampleUsername, frame.UniqueResourcesID, 1)
+	exampleProperty := test_utils.AttributeValue(t, machine_user.DescriptionVar, exampleAttributes).AsString()
+	resourceExample = strings.Replace(resourceExample, "with_secret = false", "with_secret = false\n  access_token_type = \"ACCESS_TOKEN_TYPE_JWT\"", 1)
+	test_utils.RunLifecyleTest(
+		t,
+		frame.BaseTestFrame,
+		[]string{frame.AsOrgDefaultDependency},
+		test_utils.ReplaceAll(resourceExample, exampleProperty, ""),
+		exampleProperty, "updatedproperty",
+		"", "", "",
+		false,
+		checkRemotePropertyWithAccessTokenType(frame, "ACCESS_TOKEN_TYPE_JWT"),
+		helper.ZitadelGeneratedIdOnlyRegex,
+		test_utils.CheckIsNotFoundFromPropertyCheck(checkRemoteProperty(frame), ""),
+		test_utils.ChainImportStateIdFuncs(
+			test_utils.ImportResourceId(frame.BaseTestFrame),
+			func(state *terraform.State) (string, error) {
+				return strconv.FormatBool(test_utils.AttributeValue(t, machine_user.WithSecretVar, exampleAttributes).True()), nil
+			},
+			test_utils.ImportOrgId(frame),
+		),
+	)
+}
+
+func TestAccMachineUserJWTWithSecret(t *testing.T) {
+	frame := test_utils.NewOrgTestFrame(t, "zitadel_machine_user")
+	resourceExample, exampleAttributes := test_utils.ReadExample(t, test_utils.Resources, frame.ResourceType)
+	exampleUsername := test_utils.AttributeValue(t, machine_user.UserNameVar, exampleAttributes).AsString()
+	resourceExample = strings.Replace(resourceExample, exampleUsername, frame.UniqueResourcesID, 1)
+	exampleProperty := test_utils.AttributeValue(t, machine_user.DescriptionVar, exampleAttributes).AsString()
+	exampleWithSecret := strconv.FormatBool(test_utils.AttributeValue(t, machine_user.WithSecretVar, exampleAttributes).True())
+	resourceExample = strings.Replace(resourceExample, "with_secret = false", "with_secret = false\n  access_token_type = \"ACCESS_TOKEN_TYPE_JWT\"", 1)
+	test_utils.RunLifecyleTest(
+		t,
+		frame.BaseTestFrame,
+		[]string{frame.AsOrgDefaultDependency},
+		test_utils.ReplaceAll(resourceExample, exampleProperty, exampleWithSecret),
+		exampleProperty, "updatedproperty",
+		"", "true", "true",
+		false,
+		checkRemotePropertyWithSecretAndAccessTokenType(frame, "ACCESS_TOKEN_TYPE_JWT"),
 		helper.ZitadelGeneratedIdOnlyRegex,
 		test_utils.CheckIsNotFoundFromPropertyCheck(checkRemoteProperty(frame), ""),
 		test_utils.ChainImportStateIdFuncs(
@@ -88,10 +147,41 @@ func checkRemoteProperty(frame *test_utils.OrgTestFrame) func(string) resource.T
 	}
 }
 
+func checkRemotePropertyWithAccessTokenType(frame *test_utils.OrgTestFrame, expectedAccessTokenType string) func(string) resource.TestCheckFunc {
+	return func(expect string) resource.TestCheckFunc {
+		return func(state *terraform.State) error {
+			remoteResource, err := frame.GetUserByID(frame, &management.GetUserByIDRequest{Id: frame.State(state).ID})
+			if err != nil {
+				return err
+			}
+			machine := remoteResource.GetUser().GetMachine()
+			actualDescription := machine.GetDescription()
+			if actualDescription != expect {
+				return fmt.Errorf("expected %s, but got %s", expect, actualDescription)
+			}
+			actualAccessTokenType := machine.GetAccessTokenType().String()
+			if actualAccessTokenType != expectedAccessTokenType {
+				return fmt.Errorf("expected %s, but got %s", expectedAccessTokenType, actualAccessTokenType)
+			}
+			return nil
+		}
+	}
+}
+
 func checkRemotePropertyWithSecret(frame *test_utils.OrgTestFrame) func(string) resource.TestCheckFunc {
 	return func(expect string) resource.TestCheckFunc {
 		return resource.ComposeAggregateTestCheckFunc(
 			checkRemoteProperty(frame)(expect),
+			resource.TestCheckResourceAttrSet(frame.TerraformName, "client_id"),
+			resource.TestCheckResourceAttrSet(frame.TerraformName, "client_secret"),
+		)
+	}
+}
+
+func checkRemotePropertyWithSecretAndAccessTokenType(frame *test_utils.OrgTestFrame, expectedAccessTokenType string) func(string) resource.TestCheckFunc {
+	return func(expect string) resource.TestCheckFunc {
+		return resource.ComposeAggregateTestCheckFunc(
+			checkRemotePropertyWithAccessTokenType(frame, expectedAccessTokenType)(expect),
 			resource.TestCheckResourceAttrSet(frame.TerraformName, "client_id"),
 			resource.TestCheckResourceAttrSet(frame.TerraformName, "client_secret"),
 		)
