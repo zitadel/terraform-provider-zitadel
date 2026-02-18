0734bc8296d58e51e1cea65ec0f8faccc130de89
Wojciech Piekutowski <wojciech@piekutowski.net>
2026-02-18 13:34:27 +0100

    Fix custom domain create consistency and test auth

--- zitadel/instance_custom_domain/funcs.go
+++ zitadel/instance_custom_domain/funcs.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"fmt"
 	"strings"
+	"time"
 
 	"github.com/hashicorp/terraform-plugin-log/tflog"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/diag"
@@ -38,6 +39,12 @@ func create(ctx context.Context, d *schema.ResourceData, m interface{}) diag.Dia
 	}
 
 	d.SetId(fmt.Sprintf("%s/%s", instanceID, domain))
+
+	// The add call can return before list APIs reflect the new domain.
+	if err := waitUntilCustomDomainExists(ctx, client, instanceID, domain); err != nil {
+		return diag.Errorf("failed waiting for custom domain to be readable: %v", err)
+	}
+
 	return read(ctx, d, m)
 }
 
@@ -70,7 +77,7 @@ func read(ctx context.Context, d *schema.ResourceData, m interface{}) diag.Diagn
 
 	found := false
 	for _, domainEntry := range resp.GetDomains() {
-		if domainEntry.GetDomain() == domain {
+		if equalsDomain(domainEntry.GetDomain(), domain) {
 			found = true
 			break
 		}
@@ -93,6 +100,44 @@ func read(ctx context.Context, d *schema.ResourceData, m interface{}) diag.Diagn
 	}
 
 	return nil
+}
+
+func waitUntilCustomDomainExists(ctx context.Context, client instance.InstanceServiceClient, instanceID, domain string) error {
+	deadline := time.Now().Add(30 * time.Second)
+
+	for {
+		resp, err := client.ListCustomDomains(ctx, &instance.ListCustomDomainsRequest{
+			InstanceId: instanceID,
+		})
+		if err != nil {
+			return fmt.Errorf("failed to list custom domains: %w", err)
+		}
+
+		for _, domainEntry := range resp.GetDomains() {
+			if equalsDomain(domainEntry.GetDomain(), domain) {
+				return nil
+			}
+		}
+
+		if time.Now().After(deadline) {
+			return fmt.Errorf(
+				"custom domain %q was not found for instance %q before deadline %s",
+				domain,
+				instanceID,
+				deadline.Format(time.RFC3339Nano),
+			)
+		}
+
+		select {
+		case <-ctx.Done():
+			return ctx.Err()
+		case <-time.After(2 * time.Second):
+		}
+	}
+}
+
+func equalsDomain(a, b string) bool {
+	return strings.EqualFold(strings.TrimSuffix(a, "."), strings.TrimSuffix(b, "."))
 }
 
 func delete(ctx context.Context, d *schema.ResourceData, m interface{}) diag.Diagnostics {
--- zitadel/instance_custom_domain/resource_test.go
+++ zitadel/instance_custom_domain/resource_test.go
@@ -1,37 +1,50 @@
 package instance_custom_domain_test
 
 import (
+	"context"
 	"fmt"
 	"regexp"
+	"strings"
 	"testing"
 
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
+	instancev2 "github.com/zitadel/zitadel-go/v3/pkg/client/zitadel/instance/v2"
 
+	"github.com/zitadel/terraform-provider-zitadel/v2/zitadel/helper"
 	"github.com/zitadel/terraform-provider-zitadel/v2/zitadel/helper/test_utils"
 )
 
 func TestAccInstanceCustomDomain(t *testing.T) {
-	t.Skip("Skipping test - requires system-level credentials with system.domain.write permission")
-	frame := test_utils.NewInstanceTestFrame(t, "zitadel_instance_custom_domain")
+	frame := test_utils.NewSystemTestFrame(t, "zitadel_instance_custom_domain")
+
+	instanceClient, err := helper.GetInstanceClient(context.Background(), frame.ClientInfo)
+	if err != nil {
+		t.Fatalf("failed to get instance client: %v", err)
+	}
+
+	instanceResp, err := instanceClient.GetInstance(context.Background(), &instancev2.GetInstanceRequest{})
+	if err != nil {
+		t.Fatalf("failed to get instance: %v", err)
+	}
+	instanceID := instanceResp.GetInstance().GetId()
+	testID := strings.ToLower(frame.UniqueResourcesID)
 
 	resourceConfig := func(domain string, _ string) string {
 		return fmt.Sprintf(`
 resource "zitadel_instance_custom_domain" "default" {
-    instance_id = data.zitadel_instance.current.id
+    instance_id = "%s"
     domain      = "%s"
 }
-
-data "zitadel_instance" "current" {}
-`, domain)
+`, instanceID, domain)
 	}
 
 	test_utils.RunLifecyleTest(
 		t,
-		frame.BaseTestFrame,
+		*frame,
 		nil,
 		resourceConfig,
-		"login.example.com",
-		"login.example2.com",
+		fmt.Sprintf("login-%s.example.com", testID),
+		fmt.Sprintf("login-%s-2.example.com", testID),
 		"",
 		"",
 		"",
@@ -42,7 +55,7 @@ data "zitadel_instance" "current" {}
 		regexp.MustCompile(`^.+$`),
 		test_utils.CheckNothing,
 		test_utils.ChainImportStateIdFuncs(
-			test_utils.ImportResourceId(frame.BaseTestFrame),
+			test_utils.ImportResourceId(*frame),
 		),
 	)
 }
