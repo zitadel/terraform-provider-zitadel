bfbf8d2e7f3328fa53a1cf235c34ab354af16cf5
Mridang Agarwalla <mridang.agarwalla@gmail.com>
2026-03-30 22:15:16 +0700

    fix: add nil check for expiration date in read functions

--- zitadel/pat/funcs.go
+++ zitadel/pat/funcs.go
@@ -99,9 +99,13 @@ func read(ctx context.Context, d *schema.ResourceData, m interface{}) diag.Diagn
 	}
 
 	set := map[string]interface{}{
-		ExpirationDateVar: resp.GetToken().GetExpirationDate().AsTime().Format(time.RFC3339),
-		UserIDVar:         userID,
-		helper.OrgIDVar:   orgID,
+		UserIDVar:       userID,
+		helper.OrgIDVar: orgID,
+	}
+	if exp := resp.GetToken().GetExpirationDate(); exp != nil && exp.IsValid() {
+		set[ExpirationDateVar] = exp.AsTime().Format(time.RFC3339)
+	} else {
+		set[ExpirationDateVar] = ""
 	}
 	for k, v := range set {
 		if err := d.Set(k, v); err != nil {
