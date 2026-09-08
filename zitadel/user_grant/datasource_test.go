d9ed1ad51968bc68ec4dd00cd69f35849a3fc859
Mridang Agarwalla <mridang.agarwalla@gmail.com>
2026-09-08 12:09:51 +1000

    test(user_grant): cover role_key and project_grant_id filters
    Adds positive-match tests for the role_key and project_grant_id filters of
    the zitadel_user_grants datasource and asserts project_name on the project
    filter test.

--- zitadel/user_grant/datasource_test.go
+++ zitadel/user_grant/datasource_test.go
@@ -8,6 +8,7 @@ import (
 
 	"github.com/zitadel/terraform-provider-zitadel/v2/zitadel/helper/test_utils"
 	"github.com/zitadel/terraform-provider-zitadel/v2/zitadel/human_user/human_user_test_dep"
+	"github.com/zitadel/terraform-provider-zitadel/v2/zitadel/org/org_test_dep"
 	"github.com/zitadel/terraform-provider-zitadel/v2/zitadel/project/project_test_dep"
 	"github.com/zitadel/terraform-provider-zitadel/v2/zitadel/project_role/project_role_test_dep"
 )
@@ -72,12 +73,103 @@ data "zitadel_user_grants" "default" {
 		[]string{frame.AsOrgDefaultDependency, userDep},
 		nil,
 		map[string]string{
+			"user_grants.#":              "1",
+			"user_grants.0.id":           matchingGrantID,
+			"user_grants.0.project_id":   matchingProjectID,
+			"user_grants.0.project_name": "user_grants_datasource_matching_" + frame.UniqueResourcesID,
+			"user_grants.0.role_keys.#":  "1",
+			"user_grants.0.role_keys.0":  roleKey,
+			"user_grants.0.state":        "USER_GRANT_STATE_ACTIVE",
+		},
+	)
+}
+
+func TestAccUserGrantsDatasource_FilterByRoleKey(t *testing.T) {
+	frame := test_utils.NewOrgTestFrame(t, "zitadel_user_grants")
+	userDep, userID := human_user_test_dep.Create(t, frame)
+
+	matchingRoleKey := "admin_" + frame.UniqueResourcesID
+	otherRoleKey := "viewer_" + frame.UniqueResourcesID
+	_, matchingProjectID := project_test_dep.Create(t, frame, "user_grants_datasource_matching_"+frame.UniqueResourcesID)
+	project_role_test_dep.Create(t, frame, matchingProjectID, matchingRoleKey)
+	matchingGrantID := addUserGrant(t, frame, userID, matchingProjectID, matchingRoleKey)
+
+	_, otherProjectID := project_test_dep.Create(t, frame, "user_grants_datasource_other_"+frame.UniqueResourcesID)
+	project_role_test_dep.Create(t, frame, otherProjectID, otherRoleKey)
+	addUserGrant(t, frame, userID, otherProjectID, otherRoleKey)
+
+	config := fmt.Sprintf(`
+data "zitadel_user_grants" "default" {
+  org_id   = "%s"
+  user_id  = "%s"
+  role_key = "%s"
+}
+`, frame.OrgID, userID, matchingRoleKey)
+
+	test_utils.RunDatasourceTest(
+		t,
+		frame.BaseTestFrame,
+		config,
+		[]string{frame.AsOrgDefaultDependency, userDep},
+		nil,
+		map[string]string{
 			"user_grants.#":             "1",
 			"user_grants.0.id":          matchingGrantID,
 			"user_grants.0.project_id":  matchingProjectID,
-			"user_grants.0.role_keys.#": "1",
-			"user_grants.0.role_keys.0": roleKey,
-			"user_grants.0.state":       "USER_GRANT_STATE_ACTIVE",
+			"user_grants.0.role_keys.0": matchingRoleKey,
+		},
+	)
+}
+
+func TestAccUserGrantsDatasource_FilterByProjectGrant(t *testing.T) {
+	frame := test_utils.NewOrgTestFrame(t, "zitadel_user_grants")
+
+	roleKey := "role_" + frame.UniqueResourcesID
+	_, projectID := project_test_dep.Create(t, frame, "user_grants_datasource_"+frame.UniqueResourcesID)
+	project_role_test_dep.Create(t, frame, projectID, roleKey)
+	_, grantedOrgID, grantedFrame := org_test_dep.Create(t, frame, "granted_org")
+	projectGrant, err := frame.AddProjectGrant(frame, &management.AddProjectGrantRequest{
+		ProjectId:    projectID,
+		GrantedOrgId: grantedOrgID,
+		RoleKeys:     []string{roleKey},
+	})
+	if err != nil {
+		t.Fatal(err)
+	}
+	projectGrantID := projectGrant.GetGrantId()
+
+	_, userID := human_user_test_dep.Create(t, grantedFrame)
+	resp, err := grantedFrame.AddUserGrant(grantedFrame, &management.AddUserGrantRequest{
+		UserId:         userID,
+		ProjectId:      projectID,
+		ProjectGrantId: projectGrantID,
+		RoleKeys:       []string{roleKey},
+	})
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	config := fmt.Sprintf(`
+data "zitadel_user_grants" "default" {
+  org_id           = "%s"
+  user_id          = "%s"
+  project_grant_id = "%s"
+}
+`, grantedOrgID, userID, projectGrantID)
+
+	test_utils.RunDatasourceTest(
+		t,
+		frame.BaseTestFrame,
+		config,
+		[]string{frame.AsOrgDefaultDependency},
+		nil,
+		map[string]string{
+			"user_grants.#":                  "1",
+			"user_grants.0.id":               resp.GetUserGrantId(),
+			"user_grants.0.project_id":       projectID,
+			"user_grants.0.project_grant_id": projectGrantID,
+			"user_grants.0.granted_org_id":   grantedOrgID,
+			"user_grants.0.role_keys.0":      roleKey,
 		},
 	)
 }
